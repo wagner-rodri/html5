11f2f935e3e7a39b7b05709ae165056bbc8eef81
wagner-rodri <luan.frra@gmail.com>
2020-08-28 19:11:56 -0300

    Configurado Contatos
    Foram adicionados plug ins que não são de minha aurotia, pois não é o objetivo do curso ensinar a lógica em php por trás do envio de mensagens.

--- projetos/bikcraft/web/js/script.js
+++ projetos/bikcraft/web/js/script.js
@@ -14,3 +14,12 @@ new SimpleSlide ({
 if (window.SimpleAnime) {
     new SimpleAnime()
 }
+
+if (window.SimpleForm) {
+    new SimpleForm ({
+        form: ".formphp",
+        button: "#enviar",
+        erro: "<div id='form-erro'><h2>Um erro ocorreu</h2><p>Um erro ocorreu, tente para o email contato@bikcraft.com</p></div>",
+        sucesso: "<div id='form-sucesso'><h2>Formulário enviado com sucesso</h2><p>Em breve retornaremos o contato, Obrigado!</p></div>"
+    })
+}
